b3632264ea6e62d61ed4598abea0dbb5a7fc98ec
Phillip Berndt <phillip.berndt@googlemail.com>
2017-04-11 11:55:32 +0200

    patterns() has been removed

--- loginurl/urls.py
+++ loginurl/urls.py
@@ -4,12 +4,11 @@ from django.conf import settings
 
 from loginurl.views import cleanup, login
 
-urlpatterns = patterns(
-    '',
-    (r'^cleanup/$', cleanup),
+urlpatterns = [
+    url(r'^cleanup/$', cleanup),
     url(r'^(?P<key>[0-9A-Za-z]+-[a-z0-9-]+)/$', login, name='loginurl-login'),
     url(r'^$', name='loginurl-index', view=RedirectView.as_view(
         permanent=True,
         url=settings.LOGIN_URL,
     )),
-)
+]
